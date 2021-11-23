0d9aff1386dbb1ae7dd5772c3371f85b858fab5e
DM0R3N0 <39966028+DM0R3N0@users.noreply.github.com>
2021-11-23 15:27:23 -0300

    Initial Commit

---
+++ main.c
@@ -0,0 +1,64 @@
+//================================================================
+// Nome Do Arquivo: bubblesort.c
+//
+// Descricao: Implementacao do algoritmo de ordenacaoo por bubble sort
+//================================================================
+
+// Libs
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define uma constante
+#define MAX 5
+
+// Prototipo da funcao de ordenacao
+void bubbleSort (int *vetor);
+
+int
+main (int argc, char **argv)
+{
+  int i, vetor[MAX];
+
+  // MAX ou 10 valores
+  for (i = 0; i < MAX; i++)
+    {
+      printf ("Digite um valor: ");
+      scanf ("%d", &vetor[i]);
+    }
+
+  printf ("\nOrdem inicial dos itens no array:\n");
+  for (i = 0; i < MAX; i++)
+    {
+      printf ("%4d", vetor[i]);
+    }
+
+  // Chamada da funcao que ordena os valores
+  bubbleSort (vetor);
+
+  // Imprime os valores ordenados
+  printf ("\nElementos do array em ordem crescente:\n");
+  for (i = 0; i < MAX; i++)
+    {
+      printf ("%4d", vetor[i]);
+    }
+}
+
+// Funcao de Ordenacao por Insercao
+void
+bubbleSort (int *vetor)
+{
+  int i, contador, aux;
+
+  for (contador = 1; contador < MAX; contador++)
+    {
+      for (i = 0; i < MAX - 1; i++)
+	{
+	  if (vetor[i] > vetor[i + 1])
+	    {
+	      aux = vetor[i];
+	      vetor[i] = vetor[i + 1];
+	      vetor[i + 1] = aux;
+	    }
+	}
+    }
+}
