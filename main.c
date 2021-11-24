2acbb726aaf51c4b6f526ce9e4f9e830ea662002
DM0R3N0 <39966028+DM0R3N0@users.noreply.github.com>
2021-11-24 09:23:39 -0300

    incluido função while para otimizar ordenação

--- main.c
+++ main.c
@@ -1,8 +1,4 @@
-//================================================================
-// Nome Do Arquivo: bubblesort.c
-//
-// Descricao: Implementacao do algoritmo de ordenacaoo por bubble sort
-//================================================================
+
 
 // Libs
 #include <stdio.h>
@@ -14,26 +10,33 @@
 // Prototipo da funcao de ordenacao
 void bubbleSort (int *vetor);
 
+
 int
 main (int argc, char **argv)
 {
   int i, vetor[MAX];
 
+
   // MAX ou 10 valores
   for (i = 0; i < MAX; i++)
+
     {
       printf ("Digite um valor: ");
       scanf ("%d", &vetor[i]);
     }
 
+
   printf ("\nOrdem inicial dos itens no array:\n");
+
   for (i = 0; i < MAX; i++)
     {
       printf ("%4d", vetor[i]);
     }
 
+
   // Chamada da funcao que ordena os valores
   bubbleSort (vetor);
+
 
   // Imprime os valores ordenados
   printf ("\nElementos do array em ordem crescente:\n");
@@ -47,18 +50,25 @@ main (int argc, char **argv)
 void
 bubbleSort (int *vetor)
 {
-  int i, contador, aux;
 
-  for (contador = 1; contador < MAX; contador++)
+  int i, contador, aux, troca = 1;
+  while (troca == 1) // Condição para finalizar o laço caso não haja mais trocas a serem feitas
     {
-      for (i = 0; i < MAX - 1; i++)
+      troca = 0;
+      for (contador = 1; contador < MAX; contador++)
 	{
-	  if (vetor[i] > vetor[i + 1])
+
+	  for (i = 0; i < MAX - 1; i++)
 	    {
-	      aux = vetor[i];
-	      vetor[i] = vetor[i + 1];
-	      vetor[i + 1] = aux;
+	      if (vetor[i] > vetor[i + 1])
+		{
+		  troca = 1; 
+		  aux = vetor[i];
+		  vetor[i] = vetor[i + 1];
+		  vetor[i + 1] = aux;
+		}
 	    }
 	}
     }
+
 }
